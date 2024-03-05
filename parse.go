0974459bbe11f04226fba1d456d87e3f8a6f6e92
mateusPPinh <rawrenete@gmail.com>
2024-03-04 22:19:42 -0300

    more execeptions

--- parse.go
+++ parse.go
@@ -1,10 +1,11 @@
 package main
 
 type BlockData struct {
-	Layout    string     `json:"layout"`
-	Template  string     `json:"template"`
-	MenuItems []MenuItem `json:"menuItems"`
-	Content   *Content   `json:"content"`
+	Layout       string        `json:"layout"`
+	Template     string        `json:"template"`
+	MenuItems    []MenuItem    `json:"menuItems"`
+	Content      *Content      `json:"content"`
+	CenterMiddle *CenterMiddle `json:"centerMiddle"`
 }
 
 type MenuItem struct {
@@ -18,10 +19,9 @@ type Content struct {
 	BlocksData    ContentData `json:"blocksData"`
 }
 type PageBlock struct {
-	BlockPosition int       `json:"blockPosition"`
-	PageId        string    `json:"pageId"`
-	BlockType     string    `json:"blockType"`
-	BlocksData    BlockData `json:"blocksData"`
+	PageId     string    `json:"pageId"`
+	BlockType  string    `json:"blockType"`
+	BlocksData BlockData `json:"blocksData"`
 }
 
 type SimplifiedPageBlock struct {
@@ -29,12 +29,13 @@ type SimplifiedPageBlock struct {
 }
 
 type SimplifiedBlockData struct {
-	BlockPosition int          `json:"blockPosition"`
-	BlockType     string       `json:"blockType"`
-	Layout        string       `json:"layout"`
-	Template      string       `json:"template"`
-	MenuItems     []MenuItem   `json:"menuItems,omitempty"`
-	Content       *ContentData `json:"content,omitempty"`
+	BlockPosition int           `json:"blockPosition"`
+	BlockType     string        `json:"blockType"`
+	Layout        string        `json:"layout"`
+	Template      string        `json:"template"`
+	MenuItems     []MenuItem    `json:"menuItems,omitempty"`
+	Content       *ContentData  `json:"content,omitempty"`
+	CenterMiddle  *CenterMiddle `json:"centerMiddle,omitempty"`
 }
 
 type ContentData struct {
@@ -43,42 +44,80 @@ type ContentData struct {
 	Items    []Item `json:"items,omitempty"`
 }
 
+type CenterMiddle struct {
+	BlockPosition int                `json:"blockPosition"`
+	Layout        string             `json:"layout"`
+	Template      string             `json:"template"`
+	Items         []CenterMiddleItem `json:"items,omitempty"`
+}
+
+type CenterMiddleItem struct {
+	Title    string               `json:"title,omitempty"`
+	Subtitle string               `json:"subtitle,omitempty"`
+	Images   []CenterMiddleImage  `json:"images,omitempty"`
+	Footer   []CenterMiddleFooter `json:"footer,omitempty"`
+}
+
+type CenterMiddleImage struct {
+	ImageMiddleDesktopPath string `json:"image_middle_desktop_path,omitempty"`
+	ImageMobilePath        string `json:"image_mobile_path,omitempty"`
+}
+
+type CenterMiddleFooter struct {
+	TitleTop    string `json:"title_top,omitempty"`
+	TitleBottom string `json:"title_bottom,omitempty"`
+}
+
 type Item struct {
 	CarrouselImagePath string `json:"carrousel_image_path"`
 	CarrouselImageAlt  string `json:"carrousel_image_alt"`
 }
 
+func transformCenterMiddle(centerMiddle *CenterMiddle) *CenterMiddle {
+	items := transformCenterMiddleItems(centerMiddle.Items)
+	return &CenterMiddle{
+		BlockPosition: centerMiddle.BlockPosition,
+		Layout:        centerMiddle.Layout,
+		Template:      centerMiddle.Template,
+		Items:         items,
+	}
+}
+
 func transformToSimplifiedBlocks(pageBlocks []PageBlock) []SimplifiedPageBlock {
 	var simplifiedBlocks []SimplifiedPageBlock
-	for _, block := range pageBlocks {
-		var items []Item
-		var contentData *ContentData
 
-		if block.BlocksData.Content != nil {
-			for _, item := range block.BlocksData.Content.BlocksData.Items {
-				items = append(items, Item{
-					CarrouselImagePath: item.CarrouselImagePath,
-					CarrouselImageAlt:  item.CarrouselImageAlt,
-				})
-			}
-			contentData = &ContentData{
-				Layout:   block.BlocksData.Content.BlocksData.Layout,
-				Template: block.BlocksData.Content.BlocksData.Template,
-				Items:    items,
-			}
+	for _, block := range pageBlocks {
+		var contentData *ContentData
+		var centerMiddle *CenterMiddle
+		if block.BlocksData.CenterMiddle != nil {
+			centerMiddle = transformCenterMiddle(block.BlocksData.CenterMiddle)
 		}
 
 		simplifiedBlock := SimplifiedPageBlock{
 			BlocksData: SimplifiedBlockData{
-				BlockPosition: block.BlockPosition,
-				BlockType:     block.BlockType,
-				Layout:        block.BlocksData.Layout,
-				Template:      block.BlocksData.Template,
-				MenuItems:     block.BlocksData.MenuItems,
-				Content:       contentData,
+				BlockType:    block.BlockType,
+				Layout:       block.BlocksData.Layout,
+				Template:     block.BlocksData.Template,
+				MenuItems:    block.BlocksData.MenuItems,
+				Content:      contentData,
+				CenterMiddle: centerMiddle,
 			},
 		}
 		simplifiedBlocks = append(simplifiedBlocks, simplifiedBlock)
 	}
 	return simplifiedBlocks
+}
+
+func transformCenterMiddleItems(items []CenterMiddleItem) []CenterMiddleItem {
+	var transformedItems []CenterMiddleItem
+	for _, item := range items {
+		transformedItem := CenterMiddleItem{
+			Title:    item.Title,
+			Subtitle: item.Subtitle,
+			Images:   item.Images,
+			Footer:   item.Footer,
+		}
+		transformedItems = append(transformedItems, transformedItem)
+	}
+	return transformedItems
 }
